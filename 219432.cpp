397f063f1d2e654db78d75a1e3a8794003d3f4a4
pervez <phparvez71@gmail.com>
2024-02-22 17:16:39 +0600

    while loop work

---
+++ 219432.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+  int pass=1999,x;
+
+      while (cin>>x)
+        {
+          if(x==pass){
+               cout<<"Correct"<<endl;
+               break;
+             }
+              else
+              cout<<"Wrong"<<endl;
+       
+            }
+            
+              
+
+        }
+ 
