daa4ae445b23a3e79b987a03aeca496e21270fba
pervez <phparvez71@gmail.com>
2024-02-26 22:49:24 +0600

    ok

--- 219432.cpp
+++ 219432.cpp
@@ -2,10 +2,29 @@
 using namespace std;
 
 int main(){
-
-
-
-
+  int t,n,i,fact=1;
+  cin>>t;
+  if (t>=0 && t<=15)
+  {
+    for (int i=0; i <t; i++)
+  {
+    cin>>n;
+    if(n>=0 && n<=20){
+    for ( i=1; i<=n; i++)
+    {
+      fact=fact*i;
+    }
+    }
+    cout<<fact<<endl;
+    
+  }
+  }
   
+  
+  
+
+
+
+
 }
  
