bb6dcf2850f14b45985b158f6d67d6669d919d14
pervez <phparvez71@gmail.com>
2024-02-26 22:38:38 +0600

    ok

--- 219432.cpp
+++ 219432.cpp
@@ -3,21 +3,9 @@ using namespace std;
 
 int main(){
 
-  int pass=1999,x;
-        while (cin>>x) //int x
-                      //cin>>x
-                        //while(x){} - this is inifite loop
-        {
-          if(x==pass){
-               cout<<"Correct"<<endl;
-               break;
-             }
-              else
-              cout<<"Wrong"<<endl;
-       
-            }
-            
-              
 
-        }
+
+
+  
+}
  
